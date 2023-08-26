040fffcd521d89b4924558d93208324d898c0978
vikyw89 <vikyw89@gmail.com>
2023-08-27 00:27:13 +0800

    ci: dry run

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
  import {init} from "../src/index";
 
-export default function (request: VercelRequest, response: VercelResponse) {
+export default function handler(request: VercelRequest, response: VercelResponse) {
   init()
 }
