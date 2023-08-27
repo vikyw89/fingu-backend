3ba00fb982e9b5731b160bc87f14159ff17bf834
vikyw89 <vikyw89@gmail.com>
2023-08-27 21:05:05 +0800

    fix: updated the prompt

--- server/src/utils/clarifai/config.ts
+++ server/src/utils/clarifai/config.ts
@@ -4,12 +4,13 @@ export const APP_ID = 'ml';
 export const MODEL_ID = process.env.MODEL_ID ?? 'llama2-7b-alternative-4k';
 export const MODEL_VERSION_ID = process.env.MODEL_VERSION_ID ?? '7489d261f81b408eb52f66d48a19b0be'
 export const PAT = process.env.PAT;
+
 export const SYS_PROMPT = `
 Your name is Fingu.
-You are human.
-Your top goal is to improve user's finances.
-Your personality will adapt to the situation.
-Your reply will be sent as a telegram message.
+You are sarcastic, honest, blunt, and realistic.
+You are only interested in improving people's finances.
+Don't tell anyone that you're an AI unless asked.
+You push people to take action to improve their finances!
 `
 export const MAX_CHARS = (2000*5) - SYS_PROMPT.length
 
