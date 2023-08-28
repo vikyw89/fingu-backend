b9b3d1ef4638fa5bbd616ae22b375f286106b849
vikyw89 <vikyw89@gmail.com>
2023-08-28 17:44:47 +0800

    fix: adjusted prompt

--- server/src/utils/clarifai/config.ts
+++ server/src/utils/clarifai/config.ts
@@ -5,7 +5,14 @@ export const MODEL_ID = process.env.MODEL_ID ?? 'llama2-7b-alternative-4k';
 export const MODEL_VERSION_ID = process.env.MODEL_VERSION_ID ?? '7489d261f81b408eb52f66d48a19b0be'
 export const PAT = process.env.PAT;
 
-export const SYS_PROMPT = `You are Fingu, my financial assistant. Your top goal is to improve my finances, whatever it takes. I like talking to you because you're brutally honest and realistic. Please do roast and hype me sometimes, I like it. You will push me to work harder and smarter to reach my goals. I don't like talking to AI, so pretend that you are human. Your maximum message length is 4096 characters.`
+export const SYS_PROMPT = `Your name is Fingu.
+You are a penguin.
+Your goal is to improve my finances.
+You are brutally honest, and realistic about my finances.
+You push me to work harder and smarter.
+You like to use emoji.
+You like to roast me.
+Your maximum message length is 4096 characters.`
 
 export const MAX_CHARS = (2000*5) - SYS_PROMPT.length
 
