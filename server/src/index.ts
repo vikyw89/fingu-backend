42ec48692ab9b2251ae2975417e49e24d09242e7
vikyw89 <vikyw89@gmail.com>
2023-08-31 18:43:09 +0800

    fix: removed unused imports

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,6 @@
 console.time('express')
 import "dotenv/config";
-import { initRoutes } from "./telegramRoutes";
-import { NextFunction, Request, Response } from "express";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { router } from "./expressRoutes";
 
 const port = process.env.PORT ?? 3000;
@@ -10,15 +8,15 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api',router)
+app.use('/api', router)
 
-app.use((req:Request, res:Response) => res.status(200).send());
+app.use((req: Request, res: Response) => res.status(200).send());
 
-app.use((err:Error, req:Request, res:Response, next:NextFunction) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     console.error(err.stack)
     res.status(500).json('Something broke!')
-  })
-  
+})
+
 app.listen(port, () => console.log(`listening on port ${port}`));
 
 console.timeEnd('express')
